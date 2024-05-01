93a02ca38d7f2485e8b352ed22e042c1c514c344
Daniel Robles <129451318+Daniel121224@users.noreply.github.com>
2024-05-01 13:42:47 -0500

    Delete TiendaWebbackend/TiendaWeb - copia directory

--- TiendaWebbackend/TiendaWeb - copia/src/main/java/mintic/edu/ciclo3/backend/repositorio/ProveedorRepositorio.java
+++
@@ -1,8 +0,0 @@
-package mintic.edu.ciclo3.backend.repositorio;
-
-import mintic.edu.ciclo3.backend.modelo.Proveedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProveedorRepositorio extends JpaRepository<Proveedor, Integer>{
-    
-}
