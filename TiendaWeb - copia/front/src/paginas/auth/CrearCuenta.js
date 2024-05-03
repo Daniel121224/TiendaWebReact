f4994b79c3e679ef0a289de6e3dbaf0cb9b68057
Daniel Robles <jorge2210065@correo.uis.edu.co>
2024-05-03 00:44:02 -0500

    08

---
+++ TiendaWeb - copia/front/src/paginas/auth/CrearCuenta.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CrearCuenta = () => {
+    return ( 
+        <div>
+            Crear
+        </div>
+    );
+}
+
+export default CrearCuenta;
--- TiendaWeb - copia/front/src/paginas/auth/Login.js
+++ TiendaWeb - copia/front/src/paginas/auth/Login.js
@@ -1,10 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className="hold-transition login-page">
+            <div className="login-box">
+                <div className="login-logo">
+                    <Link to={"#"}><b>Tienda</b> WEB</Link>
+                </div>
+                <div className="card">
+                    <div className="card-body login-card-body">
+                        <p className="login-box-msg">Bienvenido, Inicie de sesion</p>
+                        <form action="../../index3.html" method="post">
+                            <div className="input-group mb-3">
+                                <input type="email" 
+                                    class="form-control" 
+                                    placeholder="Email" 
+                                    id="email"
+                                    name="email"
+                                />
+                                <div className="input-group-append">
+                                    <div className="input-group-text">
+                                        <span className="fas fa-envelope"></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="input-group mb-3">
+                                <input type="password" 
+                                    className="form-control" 
+                                    placeholder="Contraseña" 
+                                    id="password"
+                                    name="password"
+                                />
+                                <div className="input-group-append">
+                                    <div className="input-group-text">
+                                        <span className="fas fa-lock"></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="social-auth-links text-center mb-3">
+                                <button type='submit' className="btn btn-block btn-primary">
+                                  Ingresar
+                                </button>
+                                <Link to={"/crear-cuenta"} className="btn btn-block btn-danger">
+                                   Registrarse
+                                </Link>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 }
 
