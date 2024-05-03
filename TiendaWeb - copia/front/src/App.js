10b5411434bdd140ae73a51f3af3543e16cde634
Daniel Robles <jorge2210065@correo.uis.edu.co>
2024-05-03 00:00:17 -0500

    07

--- TiendaWeb - copia/front/src/App.js
+++ TiendaWeb - copia/front/src/App.js
@@ -1,7 +1,16 @@
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './paginas/auth/Login';
 
 function App() {
   return (
-    <div></div>
+    <Fragment>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Login/>}/>
+        </Routes>
+      </Router>
+    </Fragment>
   );
 }
 
